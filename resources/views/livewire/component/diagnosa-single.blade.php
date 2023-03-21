d1e4b42ea746aa5f3be1396c1510b3fc7d510a49
Teguhriyadi <ilham.teguh55@gmail.com>
2023-03-22 00:24:38 +0700

    UPDATE

--- resources/views/livewire/component/diagnosa-single.blade.php
+++ resources/views/livewire/component/diagnosa-single.blade.php
@@ -1,7 +1,7 @@
 <tr>
-    <td>{{$diagnosis->code}}</td>
-    <td>{{$diagnosis->icc_code}}</td>
-    <td>{{$diagnosis->diagnosis}}</td>
+    <td>{{ $diagnosis->code }}</td>
+    <td>{{ $diagnosis->icc_code }}</td>
+    <td>{{ $diagnosis->diagnosis }}</td>
     <td>
         <button wire:click="select" class="btn btn-primary btn-sm">Pilih</button>
     </td>
