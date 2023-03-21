1bcbdac0a393c1618e2daa681a22c249fd4422fc
icntsy <icanatasya72@gmail.com>
2023-03-21 23:00:23 +0700

    modal diagnosis

--- resources/views/livewire/component/modal-diagnosa.blade.php
+++ resources/views/livewire/component/modal-diagnosa.blade.php
@@ -27,10 +27,15 @@
                             <table class="mb-0 table table-striped table-bordered">
                                 <thead>
                                 <tr>
-                                    <th>Kode Diagnosa</th>
+                                    <th>Category</th>
+                                    <th>Subcategory</th>
+                                    <th>English Name</th>
+                                    <th>Indonesian Name</th>
+                                    <th>Action</th>
+                                    {{-- <th>Kode Diagnosa</th>
                                     <th>Kode ICD</th>
                                     <th>Diagnosa</th>
-                                    <th>Action</th>
+                                    <th>Action</th> --}}
                                 </tr>
                                 </thead>
 
